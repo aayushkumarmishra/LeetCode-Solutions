6d8867115992b1098645821e1a2f805099b64cf2
Ayush Kumar Mishra <ayushmishra9615@gmail.com>
2022-09-08 14:26:41 +0530

    Update 876-middle-of-the-linked-list.java

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -14,7 +14,7 @@ class Solution {
         ListNode slow = head;
         ListNode fast = head;
         
-        while(fast != null && fast.next != null)
+        while(fast.next != null && fast.next.next != null)
         {
             slow = slow.next;
             fast = fast.next.next;
